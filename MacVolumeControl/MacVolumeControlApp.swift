be2a15ba6aef2aad292a44165f4cb7e7afb7e289
ericcil <ericcil@163.com>
2026-05-03 22:20:19 +0800

    Initial Commit

---
+++ MacVolumeControl/MacVolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacVolumeControlApp.swift
+//  MacVolumeControl
+//
+//  Created by 赵晓阳 on 2026/5/3.
+//
+
+import SwiftUI
+
+@main
+struct MacVolumeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
